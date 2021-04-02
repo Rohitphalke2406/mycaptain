e14d72be906e12afa621e3474d2e65f1413a78aa
Rohitphalke2406 <81799042+Rohitphalke2406@users.noreply.github.com>
2021-04-02 13:46:22 +0530

    created Product of two numbers
    take any two numbers. The code will provide You product of those numbers.

---
+++ product.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   float a,b;
+   cout<<"enter two number:";
+   cin>>a>>b; 
+
+   float product = a*b;
+   cout<<product;
+
+   return 0;
+
+}
