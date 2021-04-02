07245221b9beedd15f08f47ff95d19ca5e797289
Rohitphalke2406 <81799042+Rohitphalke2406@users.noreply.github.com>
2021-04-02 13:04:43 +0530

    Created First Program
    This file shows the size of int ,float ,char, double,

---
+++ first program.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    a =12;
+
+    cout<<"Size of int = "<<sizeof(a)<<endl;
+
+    float b;
+    cout<<"Size of float = "<<sizeof(b)<<endl;
+
+    char c;
+    cout<<"Size of char = "<<sizeof(c)<<endl;
+
+    double d;
+    cout<<"Size of double = "<<sizeof(d)<<endl;
+
+    return 0;
+}
